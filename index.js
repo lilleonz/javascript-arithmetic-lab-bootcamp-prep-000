151c6edd53dc6d03839c3a3a38d92c3fa556ec7e
Leon Little <little-leon@hotmail.com>
2018-02-23 02:57:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function add(a, b) {
+<<<<<<< HEAD
   return a + b;
 }
 
@@ -30,4 +31,19 @@ function makeInt(n) {
 
 function preserveDecimal(n) {
   return parseFloat(n)
+=======
+  return a+b;
+}
+
+function subtract(number) {
+  return number - number;
+}
+
+function multiply(number) {
+  return number * number;
+}
+
+function divide(number) {
+  return number / number;
+>>>>>>> 9a321b2b53acd11c60d275807533df21fd004ec1
 }
